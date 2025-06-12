4f9465d04c4c245ebdd0e235e54a00ddc2bceb6c
Darya Drobysheva <dadrobysheva@edu.hse.ru>
2025-06-12 11:27:53 +0300

    Initial Commit

---
+++ FinanceTamer/FinanceTamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTamerApp.swift
+//  FinanceTamer
+//
+//  Created by Дарья Дробышева on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
