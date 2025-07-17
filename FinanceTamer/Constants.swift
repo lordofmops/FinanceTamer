683bd82313faa0efaecfd9caa48c59f459a58265
Darya Drobysheva <dadrobysheva@edu.hse.ru>
2025-07-17 15:09:26 +0300

    feat: add loading bank account from server

---
+++ FinanceTamer/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  FinanceTamer
+//
+//  Created by Дарья Дробышева on 17.07.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let baseURLString = "https://shmr-finance.ru/api/v1"
+    static let accountsRoute = "/accounts"
+}
--- FinanceTamer/Services/BankAccounts/BankAccountsService.swift
+++ FinanceTamer/Services/BankAccounts/BankAccountsService.swift
@@ -1,21 +1,43 @@
 import Foundation
 
 final class BankAccountsService {
-    private var account = BankAccount(
-        id: 1,
-        userId: 1,
-        name: "Основной счет",
-        balance: 15000.50,
-        currency: "RUB",
-        createdAt: DateFormatters.iso8601WithFractionalSeconds.date(from: "2025-06-12T13:46:27.099Z")!,
-        updatedAt: DateFormatters.iso8601WithFractionalSeconds.date(from: "2025-06-12T13:46:27.099Z")!
-    )
+    private var account: BankAccount?
+    
+    private let networkClient = NetworkClient.shared
+    private let urlString = Constants.baseURLString + Constants.accountsRoute
     
     func account() async throws -> BankAccount {
-        account
+        guard let url = URL(string: Constants.baseURLString + Constants.accountsRoute) else {
+            print("Failed to create account URL")
+            throw NetworkError.invalidURL
+        }
+        
+        let response: [BankAccountResponse] = try await networkClient.request(url: url, method: .get, requestBody: Optional<BankAccountRequest>.none)
+        let account = BankAccount(from: response[0])
+        
+        self.account = account
+        return account
     }
     
     func updateAccount(to updatedAccount: BankAccount) async throws {
-        account = updatedAccount
+        guard let baseUrl = URL(string: Constants.baseURLString + Constants.accountsRoute) else {
+            print("Failed to create account URL")
+            throw NetworkError.invalidURL
+        }
+        let url = baseUrl.appendingPathComponent("/\(updatedAccount.id)")
+        
+        let requestBody = BankAccountRequest(
+            name: updatedAccount.name,
+            balance: updatedAccount.balance,
+            currency: updatedAccount.currency
+        )
+        
+        let response: BankAccountResponse = try await networkClient.request(
+            url: url,
+            method: .put,
+            requestBody: requestBody
+        )
+        
+        self.account = BankAccount(from: response)
     }
 }
